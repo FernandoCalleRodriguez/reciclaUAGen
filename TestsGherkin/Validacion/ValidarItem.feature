d29a444a0a31033157f44d036e107592dc0333b0
Agulló <joseantonioagullo@gmail.com>
2020-04-29 19:16:48 +0200

    Añadidas pruebas para validar items

---
+++ TestsGherkin/Validacion/ValidarItem.feature
@@ -0,0 +1,19 @@
+﻿Feature: ValidarItem
+	Para validar un item
+	Como usuario
+	Quiero cambiar el estado del item a validado
+
+Scenario: Validar un item sin validar
+	Given Tengo un item sin validar
+	When Valido el item
+	Then El item es validado
+
+Scenario: Validar un item validado
+	Given Tengo un item validado
+	When Valido el item
+	Then No se puede validar el item
+
+Scenario: Validar un item descartado
+	Given Tengo un item descartado
+	When Valido el item
+	Then No se puede validar el item
---
+++ TestsGherkin/Validacion/ValidarItemSteps.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReciclaUAGenNHibernate.CEN.ReciclaUA;
+using ReciclaUAGenNHibernate.EN.ReciclaUA;
+using ReciclaUAGenNHibernate.Enumerated.ReciclaUA;
+using ReciclaUAGenNHibernate.Exceptions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TestsGherkin.Validacion
+{
+
+    [Binding]
+    public class ValidarItemSteps
+    {
+        public ItemCEN itemCEN = new ItemCEN();
+        public string exception = "ModelException";
+        public ItemEN item;
+        public int id;
+
+        [Before]
+        public void InitializeData()
+        {
+            id = itemCEN.Crear("Item Test", "Item Test", "IMG TEST", -1, -1);
+            item = itemCEN.BuscarPorId(id);
+        }
+
+        [After]
+        public void CleanData()
+        {
+            itemCEN.Borrar(id);
+        }
+
+        [Given(@"Tengo un item sin validar")]
+        public void GivenTengoUnItemSinValidar()
+        {
+            itemCEN.Modificar(id, item.Nombre, item.Descripcion, item.Imagen, EstadoEnum.enProceso);
+        }
+
+        [Given(@"Tengo un item validado")]
+        public void GivenTengoUnItemValidado()
+        {
+            itemCEN.Modificar(id, item.Nombre, item.Descripcion, item.Imagen, EstadoEnum.verificado);
+        }
+
+        [Given(@"Tengo un item descartado")]
+        public void GivenTengoUnItemDescartado()
+        {
+            itemCEN.Modificar(id, item.Nombre, item.Descripcion, item.Imagen, EstadoEnum.descartado);
+        }
+
+        [When(@"Valido el item")]
+        public void WhenValidoElItem()
+        {
+            try
+            {
+                itemCEN.ValidarItem(id);
+            }
+            catch (ModelException e)
+            {
+                ScenarioContext.Current.Add(exception, e);
+            }
+
+        }
+
+        [Then(@"El item es validado")]
+        public void ThenElItemEsValidado()
+        {
+            item = itemCEN.BuscarPorId(id);
+            Assert.AreEqual(EstadoEnum.verificado, item.EsValido);
+        }
+
+        [Then(@"No se puede validar el item")]
+        public void ThenNoSePuedeValidarElItem()
+        {
+            var e = ScenarioContext.Current[exception];
+            Assert.IsNotNull(e);
+        }
+    }
+}
