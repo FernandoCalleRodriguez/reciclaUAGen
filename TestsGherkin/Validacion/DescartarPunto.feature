57589d1e5d161c93b310567fd24563e0aa08cb79
Agulló <joseantonioagullo@gmail.com>
2020-04-29 20:06:39 +0200

    Añadidos tests descartar puntos

---
+++ TestsGherkin/Validacion/DescartarPunto.feature
@@ -0,0 +1,19 @@
+﻿Feature: DescartarPunto
+	Para descartar un punto
+	Como usuario
+	Quiero cambiar el estado del punto a descartado
+
+Scenario: Descartar un punto sin descartar
+	Given Tengo un punto sin descartar
+	When Descarto el punto
+	Then El punto es descartado
+
+Scenario: Descartar un punto validado
+	Given Tengo un punto ya validado
+	When Descarto el punto
+	Then No se puede descartar el punto
+
+Scenario: Descartar un punto descartado
+	Given Tengo un punto ya descartado
+	When Descarto el punto
+	Then No se puede descartar el punto
---
+++ TestsGherkin/Validacion/DescartarPuntoSteps.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReciclaUAGenNHibernate.CEN.ReciclaUA;
+using ReciclaUAGenNHibernate.EN.ReciclaUA;
+using ReciclaUAGenNHibernate.Enumerated.ReciclaUA;
+using ReciclaUAGenNHibernate.Exceptions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TestsGherkin.Validacion
+{
+    [Binding]
+    public class DescartarPuntoSteps
+    {
+        public PuntoReciclajeCEN puntoCEN = new PuntoReciclajeCEN();
+        public string exception = "ModelException";
+        public PuntoReciclajeEN punto;
+        public int id;
+
+        [Before]
+        public void InitializeData()
+        {
+            id = puntoCEN.Crear(0.0d, 0.0d, -1, null);
+            punto = puntoCEN.BuscarPorId(id);
+        }
+
+        [After]
+        public void CleanData()
+        {
+            puntoCEN.Borrar(id);
+        }
+
+        [Given(@"Tengo un punto sin descartar")]
+        public void GivenTengoUnPuntoSinDescartar()
+        {
+            puntoCEN.Modificar(id, punto.Latitud, punto.Longitud, EstadoEnum.enProceso);
+        }
+        
+        [Given(@"Tengo un punto ya validado")]
+        public void GivenTengoUnPuntoYaValidado()
+        {
+            puntoCEN.Modificar(id, punto.Latitud, punto.Longitud, EstadoEnum.verificado);
+        }
+        
+        [Given(@"Tengo un punto ya descartado")]
+        public void GivenTengoUnPuntoYaDescartado()
+        {
+            puntoCEN.Modificar(id, punto.Latitud, punto.Longitud, EstadoEnum.descartado);
+        }
+        
+        [When(@"Descarto el punto")]
+        public void WhenDescartoElPunto()
+        {
+            try
+            {
+                puntoCEN.DescartarPunto(id);
+            }
+            catch (ModelException e)
+            {
+                ScenarioContext.Current.Add(exception, e);
+            }
+        }
+        
+        [Then(@"El punto es descartado")]
+        public void ThenElPuntoEsDescartado()
+        {
+            punto = puntoCEN.BuscarPorId(id);
+            Assert.AreEqual(EstadoEnum.descartado, punto.EsValido);
+        }
+        
+        [Then(@"No se puede descartar el punto")]
+        public void ThenNoSePuedeDescartarElPunto()
+        {
+            var e = ScenarioContext.Current[exception];
+            Assert.IsNotNull(e);
+        }
+    }
+}
