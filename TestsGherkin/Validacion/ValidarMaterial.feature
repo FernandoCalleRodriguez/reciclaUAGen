055905de03723853768693259e12a156fb1260c8
Agulló <joseantonioagullo@gmail.com>
2020-04-29 19:33:29 +0200

    Añadidas pruebas para validar materiales

---
+++ TestsGherkin/Validacion/ValidarMaterial.feature
@@ -0,0 +1,19 @@
+﻿Feature: ValidarMaterial
+	Para validar un material
+	Como usuario
+	Quiero cambiar el estado del material a validado
+
+Scenario: Validar un material sin validar
+	Given Tengo un material sin validar
+	When Valido el material
+	Then El material es validado
+
+Scenario: Validar un material validado
+	Given Tengo un material validado
+	When Valido el material
+	Then No se puede validar el material
+
+Scenario: Validar un material descartado
+	Given Tengo un material descartado
+	When Valido el material
+	Then No se puede validar el material
---
+++ TestsGherkin/Validacion/ValidarMaterialSteps.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReciclaUAGenNHibernate.CEN.ReciclaUA;
+using ReciclaUAGenNHibernate.EN.ReciclaUA;
+using ReciclaUAGenNHibernate.Enumerated.ReciclaUA;
+using ReciclaUAGenNHibernate.Exceptions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TestsGherkin.Validacion
+{
+
+    [Binding]
+    public class ValidarMaterialSteps
+    {
+        public MaterialCEN materialCEN = new MaterialCEN();
+        public string exception = "ModelException";
+        public MaterialEN material;
+        public int id;
+
+        [Before]
+        public void InitializeData()
+        {
+            id = materialCEN.Crear("Material Test", TipoContenedorEnum.cristal, -1);
+            material = materialCEN.BuscarPorId(id);
+        }
+
+        [After]
+        public void CleanData()
+        {
+            materialCEN.Borrar(id);
+        }
+
+        [Given(@"Tengo un material sin validar")]
+        public void GivenTengoUnMaterialSinValidar()
+        {
+            materialCEN.Modificar(id, material.Nombre, material.Contenedor, EstadoEnum.enProceso);
+        }
+        
+        [Given(@"Tengo un material validado")]
+        public void GivenTengoUnMaterialValidado()
+        {
+            materialCEN.Modificar(id, material.Nombre, material.Contenedor, EstadoEnum.verificado);
+        }
+        
+        [Given(@"Tengo un material descartado")]
+        public void GivenTengoUnMaterialDescartado()
+        {
+            materialCEN.Modificar(id, material.Nombre, material.Contenedor, EstadoEnum.descartado);
+        }
+        
+        [When(@"Valido el material")]
+        public void WhenValidoElMaterial()
+        {
+            try
+            {
+                materialCEN.ValidarMaterial(id);
+            }
+            catch (ModelException e)
+            {
+                ScenarioContext.Current.Add(exception, e);
+            }
+        }
+        
+        [Then(@"El material es validado")]
+        public void ThenElMaterialEsValidado()
+        {
+            material = materialCEN.BuscarPorId(id);
+            Assert.AreEqual(EstadoEnum.verificado, material.EsValido);
+        }
+        
+        [Then(@"No se puede validar el material")]
+        public void ThenNoSePuedeValidarElMaterial()
+        {
+            var e = ScenarioContext.Current[exception];
+            Assert.IsNotNull(e);
+        }
+    }
+}
