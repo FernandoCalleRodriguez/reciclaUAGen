5b90efb3ba61f96898b5ccab732b26e989a04c78
Agulló <joseantonioagullo@gmail.com>
2020-04-30 18:15:39 +0200

    Añadidos tests ultima respuesta de una duda

---
+++ TestsGherkin/Foro/ObtenerUltimaRespuesta.feature
@@ -0,0 +1,16 @@
+﻿Feature: ObtenerUltimaRespuesta
+	Para obtener la ultima respuesta
+	Como usuario
+	Quiero obtener la ultima respuesta publicada en una duda
+
+@ObtenerUltimaRespuesta
+Scenario: Obtener la última respuesta de una duda
+	Given Tengo una duda con respuesta
+	When Obtengo la ultima respuesta
+	Then Obtengo la ultima respuesta de la duda
+
+@ObtenerUltimaRespuesta
+Scenario: Obtener la última respuesta de una duda sin respuestas
+	Given Tengo una duda sin respuestas
+	When Obtengo la ultima respuesta
+	Then No obtengo ninguna respuesta
---
+++ TestsGherkin/Foro/ObtenerUltimaRespuestaSteps.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReciclaUAGenNHibernate.CEN.ReciclaUA;
+using ReciclaUAGenNHibernate.CP.ReciclaUA;
+using ReciclaUAGenNHibernate.EN.ReciclaUA;
+using ReciclaUAGenNHibernate.Enumerated.ReciclaUA;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace TestsGherkin.Foro
+{
+    [Binding]
+    public class ObtenerUltimaRespuestaSteps
+    {
+        public static RespuestaCEN respuestaCEN = new RespuestaCEN();
+        public static DudaCEN dudaCEN = new DudaCEN();
+        public string exception = "ModelException";
+        public static RespuestaEN respuesta;
+        public static List<int> ids = new List<int>();
+        public static int id_duda;
+
+        [Before(tags: "ObtenerUltimaRespuesta")]
+        public static void InitializeData()
+        {
+            id_duda = dudaCEN.Crear("Duda Test", "Duda Test", -1, TemaEnum.anecdota);
+        }
+
+        [After(tags: "ObtenerUltimaRespuesta")]
+        public static void CleanData()
+        {
+            dudaCEN.Borrar(id_duda);
+        }
+
+
+        [Given(@"Tengo una duda con respuesta")]
+        public void GivenTengoUnaDudaConRespuesta()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int id = respuestaCEN.Crear("Respuesta Test", id_duda, -1);
+                ids.Add(id);
+            }
+        }
+        
+        [Given(@"Tengo una duda sin respuestas")]
+        public void GivenTengoUnaDudaSinRespuestas()
+        {
+           
+        }
+        
+        [When(@"Obtengo la ultima respuesta")]
+        public void WhenObtengoLaUltimaRespuesta()
+        {
+            respuesta = new RespuestaCP().ObtenerUltimaRespuesta(id_duda);
+        }
+        
+        [Then(@"Obtengo la ultima respuesta de la duda")]
+        public void ThenObtengoLaUltimaRespuestaDeLaDuda()
+        {
+            Assert.AreEqual(ids[ids.Count - 1], respuesta.Id);
+        }
+        
+        [Then(@"No obtengo ninguna respuesta")]
+        public void ThenNoObtengoNingunaRespuesta()
+        {
+            Assert.IsNull(respuesta);
+        }
+    }
+}
