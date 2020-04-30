6cf591963f29e3d7ea5a058d90603bf867a56ce5
Agulló <joseantonioagullo@gmail.com>
2020-04-30 19:02:32 +0200

    Añadidos tests obtener si duda tiene respuesta correcta

---
+++ TestsGherkin/Foro/ObtenerSiRespuestaCorrecta.feature
@@ -0,0 +1,16 @@
+﻿Feature: ObtenerSiRespuestaCorrecta
+	Para obtener si una duda tiene respuesta correcta
+	Como usuario
+	Quiero obtener verdadero si la duda tiene una respuesta correcta
+
+@ObtenerSiRespuestaCorrecta
+Scenario: Obtener si respuesta correcta en una duda
+	Given Tengo una duda con respuesta correcta
+	When Obtengo si respuesta correcta
+	Then Obtengo respuesta correcta como verdadero
+
+@ObtenerSiRespuestaCorrecta
+Scenario: Obtener si respuesta correcta en una duda sin respuesta correcta
+	Given Tengo una duda sin respuesta correcta
+	When Obtengo si respuesta correcta
+	Then Obtengo respuesta correcta como falso
---
+++ TestsGherkin/Foro/ObtenerSiRespuestaCorrectaSteps.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReciclaUAGenNHibernate.CEN.ReciclaUA;
+using ReciclaUAGenNHibernate.CP.ReciclaUA;
+using ReciclaUAGenNHibernate.EN.ReciclaUA;
+using ReciclaUAGenNHibernate.Enumerated.ReciclaUA;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace TestsGherkin.Foro
+{
+    [Binding]
+    public class ObtenerSiRespuestaCorrectaSteps
+    {
+        public static RespuestaCEN respuestaCEN = new RespuestaCEN();
+        public static DudaCEN dudaCEN = new DudaCEN();
+        public string exception = "ModelException";
+        public static RespuestaEN respuesta;
+        public static List<int> ids = new List<int>();
+        public static int id_duda;
+        public static bool resultado;
+
+        [Before(tags: "ObtenerSiRespuestaCorrecta")]
+        public static void InitializeData()
+        {
+            id_duda = dudaCEN.Crear("Duda Test", "Duda Test", -1, TemaEnum.anecdota);
+        }
+
+        [After(tags: "ObtenerSiRespuestaCorrecta")]
+        public static void CleanData()
+        {
+            dudaCEN.Borrar(id_duda);
+        }
+
+        [Given(@"Tengo una duda con respuesta correcta")]
+        public void GivenTengoUnaDudaConRespuestaCorrecta()
+        {
+            int id = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                id = respuestaCEN.Crear("Respuesta Test", id_duda, -1);
+                ids.Add(id);
+            }
+            RespuestaEN r = respuestaCEN.BuscarPorId(id);
+            respuestaCEN.Modificar(id, r.Cuerpo, r.Fecha, true, r.Util);
+        }
+        
+        [Given(@"Tengo una duda sin respuesta correcta")]
+        public void GivenTengoUnaDudaSinRespuestaCorrecta()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int id = respuestaCEN.Crear("Respuesta Test", id_duda, -1);
+                ids.Add(id);
+            }
+        }
+        
+        [When(@"Obtengo si respuesta correcta")]
+        public void WhenObtengoSiRespuestaCorrecta()
+        {
+            resultado = new DudaCP().ObtenerSiRespuestaValida(id_duda);
+        }
+        
+        [Then(@"Obtengo respuesta correcta como verdadero")]
+        public void ThenObtengoRespuestaCorrectaComoVerdadero()
+        {
+            Assert.IsTrue(resultado);
+        }
+        
+        [Then(@"Obtengo respuesta correcta como falso")]
+        public void ThenObtengoRespuestaCorrectaComoFalso()
+        {
+            Assert.IsFalse(resultado);
+        }
+    }
+}
