a40105c093f93be430f7701a88e754187907f01a
Agulló <joseantonioagullo@gmail.com>
2020-04-30 18:25:49 +0200

    Añadidos tests descartar respuesta correcta

---
+++ TestsGherkin/Foro/DesmarcarRespuestaCorrecta.feature
@@ -0,0 +1,16 @@
+﻿Feature: DesmarcarRespuestaCorrecta
+	Para desmarcar una respuesta como correcta
+	Como usuario
+	Quiero cambiar el estado de la respuesta a no correcta
+
+@DesmarcarRespuestaCorrecta
+Scenario: Desmarcar como correcta una respuesta correcta
+	Given Tengo una respuesta ya correcta
+	When Desmarco la respuesta como correcta
+	Then La respuesta es no correcta
+
+@DesmarcarRespuestaCorrecta
+Scenario: Desmarcar como correcta una respuesta no correcta
+	Given Tengo una respuesta incorrecta
+	When Desmarco la respuesta como correcta
+	Then La respuesta no se puede desmarcar como correcta
---
+++ TestsGherkin/Foro/DesmarcarRespuestaCorrectaSteps.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReciclaUAGenNHibernate.CEN.ReciclaUA;
+using ReciclaUAGenNHibernate.EN.ReciclaUA;
+using ReciclaUAGenNHibernate.Exceptions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TestsGherkin.Foro
+{
+    [Binding]
+    public class DesmarcarRespuestaCorrectaSteps
+    {
+        public static RespuestaCEN respuestaCEN = new RespuestaCEN();
+        public string exception = "ModelException";
+        public static RespuestaEN respuesta;
+        public static int id;
+
+        [Before(tags: "DesmarcarRespuestaCorrecta")]
+        public static void InitializeData()
+        {
+            id = respuestaCEN.Crear("Respuesta Test", -1, -1);
+            respuesta = respuestaCEN.BuscarPorId(id);
+        }
+
+        [After(tags: "DesmarcarRespuestaCorrecta")]
+        public static void CleanData()
+        {
+            respuestaCEN.Borrar(id);
+        }
+
+        [Given(@"Tengo una respuesta ya correcta")]
+        public void GivenTengoUnaRespuestaYaCorrecta()
+        {
+            respuestaCEN.Modificar(id, respuesta.Cuerpo, respuesta.Fecha, true, respuesta.Util);
+        }
+        
+        [Given(@"Tengo una respuesta incorrecta")]
+        public void GivenTengoUnaRespuestaIncorrecta()
+        {
+            respuestaCEN.Modificar(id, respuesta.Cuerpo, respuesta.Fecha, false, respuesta.Util);
+        }
+        
+        [When(@"Desmarco la respuesta como correcta")]
+        public void WhenDesmarcoLaRespuestaComoCorrecta()
+        {
+            try
+            {
+                respuestaCEN.DescartarRespuestaCorrecta(id);
+            }
+            catch (ModelException e)
+            {
+                ScenarioContext.Current.Add(exception, e);
+            }
+        }
+        
+        [Then(@"La respuesta es no correcta")]
+        public void ThenLaRespuestaEsNoCorrecta()
+        {
+            respuesta = respuestaCEN.BuscarPorId(id);
+            Assert.AreEqual(false, respuesta.EsCorrecta);
+        }
+        
+        [Then(@"La respuesta no se puede desmarcar como correcta")]
+        public void ThenLaRespuestaNoSePuedeDesmarcarComoCorrecta()
+        {
+            var e = ScenarioContext.Current[exception];
+            Assert.IsNotNull(e);
+        }
+    }
+}
